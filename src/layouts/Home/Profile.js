baa920450b5732e54d453557ec58401ea20dfd0a
MolarFox <molar@molarfox.io>
2023-04-19 19:46:28 +1000

    Comment out paragraph linking gallery and uses page until those are completed

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -26,9 +26,9 @@ const ProfileText = ({ visible, titleId }) => (
     <Text className={styles.description} data-visible={visible} size="l" as="p">
       In my spare time you&apos;ll find me hiking up hills with my QRP radio gear, 3D printing my growing army of plastic foxes, playing <Link href="https://steamcommunity.com/id/MolarFox/">video games</Link>, listening to <Link href="https://bandcamp.com/molarfox">music</Link>, and building stuff in my workshop.
     </Text>
-    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    {/* <Text className={styles.description} data-visible={visible} size="l" as="p">
       Here&apos;s my <Link href="/404">gallery</Link>, my <Link href="https://blog.molarfox.io/">blog</Link>, and my list of <Link href="/404">preferred tools and gear</Link>.
-    </Text>
+    </Text> */}
   </Fragment>
 );
 
